e8483bc445b84aa52319e18e0d09b9b4a9215e9a
Joe Schmitt <joschmit@microsoft.com>
2023-02-15 16:15:06 -0800

    Update AddressListenResultsStartupLogger.cs

--- src/Tools/dotnet-monitor/AddressListenResultsStartupLogger.cs
+++ src/Tools/dotnet-monitor/AddressListenResultsStartupLogger.cs
@@ -1,12 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
+using System.IO;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Threading;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Diagnostics.Tools.Monitor
 {
