696f3a6428acc8b1fadf6b4ce2889241bde9c44d
Joe Schmitt <joschmit@microsoft.com>
2023-02-15 17:14:58 -0800

    Update EgressProvider.cs

--- src/Tools/dotnet-monitor/Egress/EgressProvider.cs
+++ src/Tools/dotnet-monitor/Egress/EgressProvider.cs
@@ -1,12 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Logging;
 using System;
-using System.ComponentModel.DataAnnotations;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Microsoft.Diagnostics.Tools.Monitor.Egress
 {
