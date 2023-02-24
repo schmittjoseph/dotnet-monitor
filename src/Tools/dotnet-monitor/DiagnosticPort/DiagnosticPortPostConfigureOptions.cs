ad0c69524609d4625942f3f33415618ed806da75
Joe Schmitt <joschmit@microsoft.com>
2023-02-24 14:20:05 -0800

    Update DiagnosticPortPostConfigureOptions.cs

--- src/Tools/dotnet-monitor/DiagnosticPort/DiagnosticPortPostConfigureOptions.cs
+++ src/Tools/dotnet-monitor/DiagnosticPort/DiagnosticPortPostConfigureOptions.cs
@@ -1,11 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
+using System.Runtime.InteropServices;
 using Microsoft.Diagnostics.Monitoring.WebApi;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using System.IO;
-using System.Runtime.InteropServices;
 
 namespace Microsoft.Diagnostics.Tools.Monitor
 {
