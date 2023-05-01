407cbdbe3f60c54547b5bda8eab7309b9a4f7607
Joe Schmitt <joschmit@microsoft.com>
2023-05-01 16:37:35 -0700

    Faster probes

--- src/Microsoft.Diagnostics.Monitoring.HostingStartup/Snapshotter/Probes.cs
+++ src/Microsoft.Diagnostics.Monitoring.HostingStartup/Snapshotter/Probes.cs
@@ -10,14 +10,46 @@ using Microsoft.Diagnostics.Monitoring.HostingStartup;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Collections;
+using System.Linq;
 
 namespace Microsoft.Diagnostics.Monitoring.StartupHook.Snapshotter
 {
     public static class Probes
     {
+        private readonly struct MethodCacheEntry
+        {
+            public MethodCacheEntry(
+                MethodInfo methodInfo,
+                string prettyPrintStringFormat,
+                bool[] supportedArgs,
+                bool hasImplicitThis,
+                Type? declaringType,
+                ParameterInfo[] parameters)
+            {
+                MethodInfo = methodInfo;
+                PrettyPrintStringFormat = prettyPrintStringFormat;
+                SupportedArgs = supportedArgs;
+                HasImplicitThis = hasImplicitThis;
+                DeclaringType = declaringType;
+                Parameters = parameters;
+            }
+
+            public MethodInfo MethodInfo { get; }
+
+            public bool[] SupportedArgs { get; }
+
+            public bool HasImplicitThis { get; }
+
+            public Type? DeclaringType { get; }
+
+            public ParameterInfo[] Parameters { get; }
+
+            public string PrettyPrintStringFormat { get; }
+        }
+
         public delegate void EnterProbeDelegate(long uniquifier, object[] args);
 
-        private static readonly ConcurrentDictionary<long, (MethodInfo, bool[])> methodLookup = new();
+        private static readonly ConcurrentDictionary<long, MethodCacheEntry> methodLookup = new();
 #pragma warning disable CS0649 // Field 'Probes.LogTypes' is never assigned to, and will always have its default value false
         private static bool LogTypes;
 #pragma warning restore CS0649 // Field 'Probes.LogTypes' is never assigned to, and will always have its default value false
@@ -36,11 +68,121 @@ namespace Microsoft.Diagnostics.Monitoring.StartupHook.Snapshotter
             ThreadPool.QueueUserWorkItem(ProbeHandler);
         }
 
-        public static void RegisterMethodToProbeInCache(long uniquifier, MethodInfo method, bool[] argsSupported)
+        private static void EmitEscapedString(StringBuilder stringBuilder, string value)
         {
+            stringBuilder.Append("{{");
+            stringBuilder.Append(value);
+            stringBuilder.Append("}}");
+        }
+
+        private static bool EmitParameter(StringBuilder stringBuilder, Type? type, string? name, bool isSupported, int formatIndex, ParameterInfo? paramInfo = null)
+        {
+            if (LogTypes)
+            {
+                stringBuilder.Append('(');
+                if (type == null)
+                {
+                    EmitEscapedString(stringBuilder, "unknown");
+                }
+                else
+                {
+                    stringBuilder.Append(type);
+                }
+
+                stringBuilder.Append(") ");
+            }
+
+            if (paramInfo?.IsOut == true)
+            {
+                stringBuilder.Append($"out ");
+            }
+            else if (type?.IsByRefLike == true)
+            {
+                stringBuilder.Append($"ref struct ");
+            }
+            else if (type?.IsByRef == true)
+            {
+                stringBuilder.Append($"ref ");
+            }
+
+            if (name == null)
+            {
+                EmitEscapedString(stringBuilder, "unknown");
+            }
+            else
+            {
+                stringBuilder.Append(name);
+            }
+
+            stringBuilder.Append(": ");
+
+            if (isSupported)
+            {
+                stringBuilder.Append('{');
+                stringBuilder.Append(formatIndex);
+                stringBuilder.Append('}');
+                return true;
+            }
+            else
+            {
+                EmitEscapedString(stringBuilder, "unsupported");
+                return false;
+            }
+        }
+
+        public static void RegisterMethodToProbeInCache(long uniquifier, MethodInfo method, bool[] supportedArgs)
+        {
+            StringBuilder fmtStringBuilder = new();
+
+            fmtStringBuilder.Append($"[enter] {method.Module}!");
+            string className = method.DeclaringType?.FullName?.Split('`')?[0] ?? string.Empty;
+            fmtStringBuilder.Append(className);
+            PrettyPrintGenericArgs(fmtStringBuilder, method.DeclaringType?.GetGenericArguments());
+
+            fmtStringBuilder.Append($".{method.Name}");
+            PrettyPrintGenericArgs(fmtStringBuilder, method.GetGenericArguments());
+
+            fmtStringBuilder.Append('(');
+
+            int fmtIndex = 0;
+            int index = 0;
+            ParameterInfo[] parameters = method.GetParameters();
+            if (method.CallingConvention.HasFlag(CallingConventions.HasThis))
+            {
+                if(EmitParameter(fmtStringBuilder, method.DeclaringType, "this", supportedArgs[index], fmtIndex))
+                {
+                    fmtIndex++;
+                }
+                index++;
+            }
+
+            foreach (ParameterInfo paramInfo in parameters)
+            {
+                if (fmtIndex != 0)
+                {
+                    fmtStringBuilder.Append(", ");
+                }
+
+                if (EmitParameter(fmtStringBuilder, paramInfo.ParameterType, paramInfo.Name, supportedArgs[index], fmtIndex, paramInfo))
+                {
+                    fmtIndex++;
+                }
+
+                index++;
+            }
+
+            fmtStringBuilder.Append(')');
+
             lock (Locker)
             {
-                methodLookup[uniquifier] = (method, argsSupported);
+                
+                methodLookup[uniquifier] = new MethodCacheEntry(
+                    method,
+                    fmtStringBuilder.ToString(),
+                    supportedArgs,
+                    method.CallingConvention.HasFlag(CallingConventions.HasThis),
+                    method.DeclaringType,
+                    parameters);
             }
         }
 
@@ -64,11 +206,11 @@ namespace Microsoft.Diagnostics.Monitoring.StartupHook.Snapshotter
             }
         }
 
-        private static (MethodBase, bool[])? GetMethodInformation(long uniquifier)
+        private static MethodCacheEntry? GetMethodInformation(long uniquifier)
         {
-            if (methodLookup.TryGetValue(uniquifier, out (MethodInfo, bool[]) cachedValue))
+            if (methodLookup.TryGetValue(uniquifier, out MethodCacheEntry cacheEntry))
             {
-                return cachedValue;
+                return cacheEntry;
             }
 
             return null;
@@ -82,111 +224,32 @@ namespace Microsoft.Diagnostics.Monitoring.StartupHook.Snapshotter
 
         public static void EnterProbe(long uniquifier, object[] args)
         {
-            (MethodBase, bool[])? methodInformation = GetMethodInformation(uniquifier);
-            if (!methodInformation.HasValue)
+            if (args == null)
             {
                 return;
             }
 
-            MethodBase method = methodInformation.Value.Item1;
-            bool[] argsSupported = methodInformation.Value.Item2;
+            MethodCacheEntry? cacheEntry = GetMethodInformation(uniquifier);
+            if (!cacheEntry.HasValue)
+            {
+                return;
+            }
 
-            bool hasThis = !method.IsStatic;
-
-            StringBuilder stringBuilder = new StringBuilder();
-            StringBuilder argValueBuilder = new StringBuilder();
-
-            // JSFIX: Cache this
-            stringBuilder.Append($"[enter] {method.Module}!");
-            string className = method.DeclaringType?.FullName?.Split('`')?[0] ?? string.Empty;
-            stringBuilder.Append(className);
-            PrettyPrintGenericArgs(stringBuilder, method.DeclaringType?.GetGenericArguments());
-
-            stringBuilder.Append($".{method.Name}");
-            PrettyPrintGenericArgs(stringBuilder, method.GetGenericArguments());
-
-            stringBuilder.Append('(');
-            var parameters = method.GetParameters(); // JSFIX: Cache
+            StringBuilder argBuilder = new();
+            string[] argValues = new string[args.Length];
             for (int i = 0; i < args?.Length; i++)
             {
-                try
+                if (!cacheEntry.Value.SupportedArgs[i])
                 {
-                    if (i != 0)
-                    {
-                        stringBuilder.Append(", ");
-                    }
-
-                    if (hasThis && i == 0)
-                    {
-                        if (LogTypes)
-                        {
-                            stringBuilder.Append($"({args[i].GetType()}) ");
-                        }
-                        stringBuilder.Append("this");
-                    }
-                    else
-                    {
-                        int paramI = (hasThis) ? i - 1 : i;
-                        var paramInfo = parameters[paramI];
-                        if (LogTypes)
-                        {
-                            stringBuilder.Append($"({paramInfo.ParameterType}) ");
-                        }
-
-                        if (paramInfo.IsOut)
-                        {
-                            stringBuilder.Append($"out ");
-                        }
-                        else if (paramInfo.ParameterType.IsByRefLike)
-                        {
-                            stringBuilder.Append($"ref struct ");
-                        }
-                        else if (paramInfo.ParameterType.IsByRef)
-                        {
-                            stringBuilder.Append($"ref ");
-                        }
-
-                        stringBuilder.Append(paramInfo.Name);
-                    }
-                    stringBuilder.Append(": ");
-
-                    if (!argsSupported[i])
-                    {
-                        stringBuilder.Append("{unsupported}");
-                        continue;
-                    }
-
-                    argValueBuilder.Clear();
-                    try
-                    {
-                        Type? type = null;
-                        int paramI = (hasThis) ? i - 1 : i;
-                        if (paramI >= 0)
-                        {
-                            type = parameters[paramI].ParameterType;
-                        }
-                        else
-                        {
-                            type = method.DeclaringType;
-                        }
-
-                        SerializeObject(argValueBuilder, args[i]);
-                        stringBuilder.Append(argValueBuilder);
-                    }
-                    catch (Exception ex)
-                    {
-                        stringBuilder.Append($"internal error: {ex}");
-                    }
+                    continue;
                 }
-                catch (Exception ex)
-                {
-                    InProcLoggerService.Log($"Internal hook error: {ex}", LogLevel.Critical);
-                    requestProbeStopEvent.Set();
-                }
+
+                SerializeObject(argBuilder, args[i]);
+                argValues[i] = argBuilder.ToString();
+                argBuilder.Clear();
             }
-            stringBuilder.Append(')');
 
-            InProcLoggerService.Log(stringBuilder.ToString());
+            InProcLoggerService.Log(string.Format(cacheEntry.Value.PrettyPrintStringFormat, argValues));
 
             if (CountDown > 0)
             {
