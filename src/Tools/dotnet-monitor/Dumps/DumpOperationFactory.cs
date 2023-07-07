39a45122c33915f25cb609b76cbf10f4f8dee2de
Joe Schmitt <1146681+schmittjoseph@users.noreply.github.com>
2023-07-07 10:22:50 -0700

    Update DumpOperationFactory.cs

--- src/Tools/dotnet-monitor/Dumps/DumpOperationFactory.cs
+++ src/Tools/dotnet-monitor/Dumps/DumpOperationFactory.cs
@@ -1,9 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.Extensions.Logging;
 using Microsoft.Diagnostics.Monitoring.WebApi;
 using Microsoft.Diagnostics.Monitoring.WebApi.Models;
-using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Diagnostics.Tools.Monitor
 {
