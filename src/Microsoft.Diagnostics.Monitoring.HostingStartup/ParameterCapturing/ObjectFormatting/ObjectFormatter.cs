0c65e4fdc646b77a1583a76d7edfef7617f24f58
Joe Schmitt <joschmit@microsoft.com>
2023-09-06 10:28:22 -0700

    Update token

--- src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/ObjectFormatting/ObjectFormatter.cs
+++ src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/ObjectFormatting/ObjectFormatter.cs
@@ -30,7 +30,7 @@ namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing.Obj
             }
 
             public const string Exception = Internal.Prefix + "exception_thrown" + Internal.Postfix;
-            public const string CannotFormatWithoutSideEffects = Internal.Prefix + "cannot_format_side_effect_free" + Internal.Postfix;
+            public const string CannotFormatWithoutSideEffects = Internal.Prefix + "cannot_format_without_side_effects" + Internal.Postfix;
         }
 
         public static string WrapValue(string value) => string.Concat(
